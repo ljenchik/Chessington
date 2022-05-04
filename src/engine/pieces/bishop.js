85253ece2a9af2c5bb25a639a3d95d8f5c3eb5fc
ljenchik <pelagenkolena@gmail.com>
2022-05-04 17:28:42 +0100

    Added rook bishop moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class Bishop extends Piece {
@@ -6,6 +7,24 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let availableMoves = []
+        for (let i  = 1; i < 8; i++) {
+            // Forwards diagonal
+            if (location.row + i < 8 && location.col + i < 8) {
+                availableMoves.push(Square.at(location.row + i, location.col + i))
+            }
+            if (location.row - i >= 0 && location.col - i >= 0) {
+                availableMoves.push(Square.at(location.row - i, location.col - i))
+            }
+            // Backwards diagonal
+            if (location.row + i < 8 && location.col - i >= 0) {
+                availableMoves.push(Square.at(location.row + i, location.col - i))
+            }
+            if (location.row - i >= 0 && location.col + i < 8) {
+                availableMoves.push(Square.at(location.row - i, location.col + i))
+            }
+        }
+        return availableMoves;
     }
 }
