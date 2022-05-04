3094c419c70f8cd9b4a8d090854488e8447aabd3
ljenchik <pelagenkolena@gmail.com>
2022-05-04 16:25:13 +0100

    Added rook moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class Rook extends Piece {
@@ -6,6 +7,22 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let availableMoves = []
+        // Horisontally
+        for (let i  = location.col + 1; i < 8; i++) {
+            availableMoves.push(Square.at(location.row, i))
+        }
+        for (let i  = 0; i < location.col; i++) {
+            availableMoves.push(Square.at(location.row, i))
+        }
+        // Vertically
+        for (let j  = location.row + 1; j < 8; j++) {
+            availableMoves.push(Square.at(j, location.col))
+        }
+        for (let j = 0; j < location.row; j++) {
+            availableMoves.push(Square.at(j, location.col))
+        }
+        return availableMoves;
     }
 }
