9efbee2b54dbc35f7abb1b52a59457154093b13a
ljenchik <pelagenkolena@gmail.com>
2022-05-04 18:20:44 +0100

    Added knight moves

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,20 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let availableMoves = []
+       
+        availableMoves.push(Square.at(location.row + 2, location.col + 1))
+        availableMoves.push(Square.at(location.row + 2, location.col - 1))
+        availableMoves.push(Square.at(location.row - 2, location.col + 1))
+        availableMoves.push(Square.at(location.row - 2, location.col - 1))
+
+        availableMoves.push(Square.at(location.row + 1, location.col + 2))
+        availableMoves.push(Square.at(location.row - 1, location.col + 2))
+        availableMoves.push(Square.at(location.row + 1, location.col - 2))
+        availableMoves.push(Square.at(location.row - 1, location.col - 2))
+        
+        console.log(availableMoves);
+        return availableMoves;
     }
 }
