f8c0449afa961854a3c6d27c1e92d7efa733192f
ljenchik <pelagenkolena@gmail.com>
2022-05-07 22:02:08 +0100

    Added bishop tests

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -9,20 +9,28 @@ export default class Bishop extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         let availableMoves = []
+        // Forwards diagonal up
+        for (let i  = 1; i < 8 ; i++) {
+            if (!board.validateAndPush(location.row + i, location.col + i, availableMoves)) {
+                break;
+            }
+        }
+        // Forwards diagonal down
         for (let i  = 1; i < 8; i++) {
-            // Forwards diagonal
-            if (location.row + i < 8 && location.col + i < 8) {
-                availableMoves.push(Square.at(location.row + i, location.col + i));
+            if (!board.validateAndPush(location.row - i, location.col - i, availableMoves)) {
+                break;
             }
-            if (location.row - i >= 0 && location.col - i >= 0) {
-                availableMoves.push(Square.at(location.row - i, location.col - i));
+        }
+        // Backwards diagonal up
+        for (let i  = 1; i < 8; i++) {
+            if (!board.validateAndPush(location.row + i, location.col - i, availableMoves)) {
+                break;
             }
-            // Backwards diagonal
-            if (location.row + i < 8 && location.col - i >= 0) {
-                availableMoves.push(Square.at(location.row + i, location.col - i));
-            }
-            if (location.row - i >= 0 && location.col + i < 8) {
-                availableMoves.push(Square.at(location.row - i, location.col + i));
+        }
+        for (let i  = 1; i < 8; i++) {
+            // Backwards diagonal down
+            if (!board.validateAndPush(location.row - i, location.col + i, availableMoves)) {
+                break;
             }
         }
         return availableMoves;
