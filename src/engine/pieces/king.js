a499f59447f4fb9ffa9c1a86737c35e0711973aa
ljenchik <pelagenkolena@gmail.com>
2022-05-07 22:19:10 +0100

    Added king and knight tests

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -11,30 +11,19 @@ export default class King extends Piece {
         let location = board.findPiece(this);
         let availableMoves = []
 
-        availableMoves.push(Square.at(location.row + 1, location.col));
-        availableMoves.push(Square.at(location.row - 1, location.col));
-        availableMoves.push(Square.at(location.row, location.col + 1));
-        availableMoves.push(Square.at(location.row, location.col - 1));
+        board.validateAndPush(location.row + 1, location.col, availableMoves);
+        board.validateAndPush(location.row - 1, location.col, availableMoves);
+        board.validateAndPush(location.row, location.col + 1, availableMoves);
+        board.validateAndPush(location.row, location.col - 1, availableMoves);
 
-        availableMoves.push(Square.at(location.row + 1, location.col + 1));
-        availableMoves.push(Square.at(location.row - 1, location.col - 1));
-        availableMoves.push(Square.at(location.row + 1, location.col - 1));
-        availableMoves.push(Square.at(location.row - 1, location.col + 1));
-
-
-        /*
-        let location = board.findPiece(this);
-        let newMoves = [];
-        for (let i = -1; i < 2; i++) {
-            for (let j = -1; j < 2; j++) {
-                if(i != 0 || j != 0 && board.isValidSquare(location.row + i, location.col + j)) newMoves.push(Square.at(location.row + i, location.col + j));
-            }
-        }
-        console.log(newMoves);
-        return newMoves;
-    }*/
+        board.validateAndPush(location.row + 1, location.col + 1, availableMoves);
+        board.validateAndPush(location.row - 1, location.col - 1, availableMoves);
+        board.validateAndPush(location.row + 1, location.col - 1, availableMoves);
+        board.validateAndPush(location.row - 1, location.col + 1, availableMoves);
         
         return availableMoves;
     }
-    
 }
+        
+        
+    
